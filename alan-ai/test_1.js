5e2b01810971b56e7123b686d4d84bca0be50d3b
Sophia Zhu <sophiazhu16@gmail.com>
2022-05-25 20:50:02 -0700

    Update test_1.js

--- alan-ai/test_1.js
+++ alan-ai/test_1.js
@@ -2,3 +2,14 @@
 intent('hello world', p => {
     p.play('(hello|hi there)');
 });
+
+intent(
+  'Who\'s there',
+  'What\'s your name',
+  p => {
+      p.play(
+          'My name is Alan.',
+          'It\'s Alan.',
+      );
+  },
+);
