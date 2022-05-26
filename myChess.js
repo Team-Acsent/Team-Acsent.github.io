3985fa711ae034757897031958f3a7e5e526899a
Sophia Zhu <sophiazhu16@gmail.com>
2022-05-25 22:30:51 -0700

    properly installed chess.js using npm

---
+++ myChess.js
@@ -0,0 +1,10 @@
+import { Chess } from '/logic/chess.js'
+
+const chess = new Chess()
+
+var board = document.getElementById("ascii-board");
+chess.move("e4");
+chess.move("c5");
+board.innerHTML = chess.ascii();
+console.log(chess.ascii);
+console.log(chess.pgn())
